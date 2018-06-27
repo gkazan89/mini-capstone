1e3db01d44a2de1b5ee5800ada2faf116397e62c
Grant Kazan <grantkazan@gmail.com>
2018-06-26 19:19:31 -0500

    can display json data!

--- app/controllers/guitars_controller.rb
+++ app/controllers/guitars_controller.rb
@@ -2,4 +2,9 @@ class GuitarsController < ApplicationController
   def hello_method
     render json: {hello: "dr owl"}
   end
+
+  def guitar_method
+    @guitars = Guitar.all
+    render "guitars.json.jbuilder"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/hello_url" => "guitars#hello_method"
+  get "/guitars_url" => "guitars#guitar_method"
 end
