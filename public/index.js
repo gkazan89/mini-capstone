33f3c901674fdcc2f59d8bfb4a9aa0b99593d511
Grant Kazan <grantkazan@gmail.com>
2018-07-29 09:27:19 -0500

    use JS to paste repeating HTML

---
+++ public/index.js
@@ -0,0 +1,8 @@
+var guitarTemplate = document.querySelector("#guitar-card");
+var guitarContainer = document.querySelector(".container");
+
+guitarContainer.appendChild(guitarTemplate.content.cloneNode(true));
+guitarContainer.appendChild(guitarTemplate.content.cloneNode(true));
+guitarContainer.appendChild(guitarTemplate.content.cloneNode(true));
+guitarContainer.appendChild(guitarTemplate.content.cloneNode(true));
+guitarContainer.appendChild(guitarTemplate.content.cloneNode(true));
