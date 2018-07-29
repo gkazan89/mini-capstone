e2bdccbc5dfe4a69f2d0424104a6fca2af226ae3
Grant Kazan <grantkazan@gmail.com>
2018-07-29 10:17:50 -0500

    use axios to make AJAX request for data and display

--- public/index.js
+++ public/index.js
@@ -1,8 +1,18 @@
+/* global axios */
+
 var guitarTemplate = document.querySelector("#guitar-card");
 var guitarContainer = document.querySelector(".container");
 
-guitarContainer.appendChild(guitarTemplate.content.cloneNode(true));
-guitarContainer.appendChild(guitarTemplate.content.cloneNode(true));
-guitarContainer.appendChild(guitarTemplate.content.cloneNode(true));
-guitarContainer.appendChild(guitarTemplate.content.cloneNode(true));
-guitarContainer.appendChild(guitarTemplate.content.cloneNode(true));
+axios.get("http://localhost:3000/api/guitars").then(function(response) {
+  var guitars = response.data;
+  guitars.forEach(function(guitar) {
+    console.log(guitar);
+    var guitarClone = guitarTemplate.content.cloneNode(true);
+    guitarClone.querySelector(".card-title").innerText = guitar.name;
+    guitarClone.querySelector(".card-text").innerText = guitar.description;
+    guitarClone.querySelector("#price").innerText = guitar.price;
+    guitarClone.querySelector(".card-img-top").src = guitar.images[0].url;
+    console.log(guitar.images[0]);
+    guitarContainer.appendChild(guitarClone);
+  });
+});
